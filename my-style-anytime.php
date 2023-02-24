4dab073ca2950a48b277544c0082033c3fce966b
NewfieSoft <profile@newfiesoft.com>
2023-02-25 00:13:48 +0100

    Add files via upload
    Release new version 1.0.4

--- my-style-anytime.php
+++ my-style-anytime.php
@@ -1,23 +1,36 @@
 <?php
 
-/*
-
+/**
 Plugin Name: My Style Anytime
 Plugin URI: https://newfiesoft.com/wp-plugins/my-style-anytime/
 
-Description: This WordPress plugin helps you to create and customize Front-end <strong>(public view)</strong> and Back-end <strong>(wp-admin)</strong> style views using the user's type of rules. At the same time, you can do responsive design on the same CSS file rule. <strong># Lite Version</strong>
+Description: This WordPress plugin helps you to create and customize Front-end <strong>(public view)</strong> and Back-end <strong>(wp-admin)</strong> style views using the user's type of rules. At the same time, you can do responsive design on the same CSS file rule. <strong>#Lite Version</strong> Now you can <strong>Disable Gutenberg style</strong> anywhere & Enable the classic editor. This function helps you to back your WordPress in a classic view in posts, pages, widgets
 
-Version: 1.0.3
+Version: 1.0.4
 Author: NewfieSoft
 Author URI: https://www.newfiesoft.com
 Donate link: https://newfiesoft.com/donate
 
 Text Domain: my-style-anytime
+Domain Path: /languages/
 
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+defined('ABSPATH') or exit();
+
+if ( ! function_exists( 'is_plugin_active' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+// Load plugin Text Domain
+add_action( 'plugins_loaded', 'mmysat_plugin_load_text_domain' );
+
+function mmysat_plugin_load_text_domain() {
+	load_plugin_textdomain( 'my-style-anytime', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+ //	 error_log( print_r( is_textdomain_loaded( 'my-style-anytime' ), true ) );
+}
 
 
 // This is plugin custom style.css
@@ -35,7 +48,7 @@ function mysat_active_admin_menu()
 {
     add_menu_page(
         'General',
-        'My Style Anytime', // Menu Title
+	    __('My Style Anytime', 'my-style-anytime'), // Menu Title;
         'activate_plugins',
         'my-style-anytime', // ID
         'mysat_general_info_page',
@@ -45,7 +58,7 @@ function mysat_active_admin_menu()
     add_submenu_page( // General button
         'my-style-anytime', // ID
         'General',
-        'General', // Menu Title
+	    __('General', 'my-style-anytime'), // Menu Title;
         'activate_plugins',
         'my-style-anytime', // Your menu URL slug
         'mysat_general_info_page' // echo function
@@ -54,58 +67,76 @@ function mysat_active_admin_menu()
     add_submenu_page( // Manage Style button
         'my-style-anytime', // ID
         'Manage',
-        'Manage Style', // Menu Title
+	    __('Manage Style', 'my-style-anytime'), // Menu Title;
         'activate_plugins',
         'plugin-editor.php?file=my-style-anytime%2Fstyles%2Fadministrator-style.css&plugin=my-style-anytime%2Fmy-style-anytime.php',
     // Your menu URL slug
     );
 
+    add_submenu_page( // Settings button
+        'my-style-anytime', // ID
+        'Settings',
+	    __('Settings', 'my-style-anytime'), // Menu Title;
+        'activate_plugins',
+        'mysat_settings_page', // Your menu URL slug
+	    'mysat_render_settings_page' // callback function
+    );
+
     add_submenu_page(  // Help button
         'my-style-anytime', // ID
         'Help',
-        'Help', // Menu Title
+	    __('Help', 'my-style-anytime'), // Menu Title;
         'activate_plugins',
         'help', // Your menu URL slug
         static function () { // callback function
-            require "help/help.php";
+            require "pages/mysat-help.php";
         }
     );
 }
 
+add_action('', 'mysat_general_info_page');
 function mysat_general_info_page()
 {
 
+// Get My plugin version
+	if ( is_admin() ) {
+		if ( ! function_exists( 'get_plugin_data' ) ) {
+			require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		}
+		$plugin_data = get_plugin_data( __FILE__ );
+		printf( '<div class="version">%s %s</div>', __('Version: ', 'my-style-anytime'), $plugin_data['Version'] );
+	}
+
     ?>
-
-    <div class="license-container">
-        <h3 class="license-title" style="margin:0;"><i class="dashicons dashicons-info-outline"></i> Information
-        </h3>
+    <div class="license-container" style="margin-top: 2px;">
+        <h3 class="license-title" style="margin:0;"><i class="dashicons dashicons-info-outline"></i> <?php _e( 'Welcome', 'my-style-anytime' ); ?></h3>
         <hr>
-        <p style="padding-top:10px;">Hi You,</p>
-        <p>We are honored that you decided to use our plugin.</p>
-        <p>This is <strong class="lite">Lite Version</strong>, and you have full WordPress rules support.</p>
-        <p>If you like this plugin and you need other roles in plugins like as is WooCommerce or others who have user
-            roles.</p>
+        <p style="padding-top:10px;"><?php _e( 'Hi,', 'my-style-anytime' ); ?></p>
+        <p><?php _e( 'We are honored that you decided to use our plugin.', 'my-style-anytime' ); ?></p>
+        <p><?php _e( 'This is', 'my-style-anytime' ); ?><strong class="lite">
+           <?php _e( 'Lite Version,', 'my-style-anytime' ); ?></strong>
+        <?php _e( 'and you have full WordPress default user rules support.', 'my-style-anytime' ); ?></p>
+        <p><?php _e( 'If you like this plugin and you need other plugins user roles in plugins like as is WooCommerce or others who have user roles', 'my-style-anytime' ); ?>.</p>
         <p>
-            Visit our site for a premium offer <a href="https://newfiesoft.com/plugins/my-style-anytime/"
-                                                  target="_blank">here</a>, for
-            a single or multi-site with one single license key.</p>
+	        <?php _e( 'Visit our site for a premium offer', 'my-style-anytime' ); ?>
+          <a href="https://newfiesoft.com/plugins/my-style-anytime/" target="_blank"><?php _e( 'here', 'my-style-anytime' ); ?></a>
+      <?php _e( ', for a single or multi-site with one single license key', 'my-style-anytime' ); ?>.</p>
         <br>
-        Enjoy your job... :)
+	    <?php _e( 'Enjoy your work', 'my-style-anytime' ); ?>... 🥳
     </div>
 
-    <div class="power-by-info" style="width: 98%;text-align: right;">Premium Tools for WordPress made by <a
-                href="https://www.newfiesoft.com" target="_blank">NewfieSoft</a> with <img draggable="false" role="img"
-                                                                                           class="emoji" alt="❤"
-                                                                                           src="https://s.w.org/images/core/emoji/14.0.0/svg/2764.svg">
-        in Zürich, Switzerland.
-    </div>
+    <div class="power-by-info"><?php _e( 'Premium Tools for WordPress made by', 'my-style-anytime' ); ?> <a href="https://www.newfiesoft.com" target="_blank">NewfieSoft</a>
+        <?php _e( 'with', 'my-style-anytime' ); ?> <img draggable="false" role="img" class="emoji" alt="❤" src="https://s.w.org/images/core/emoji/14.0.0/svg/2764.svg">
+        <?php _e( 'in Zürich, Switzerland', 'my-style-anytime' ); ?>.</div>
 
     <?php
 
 }
 
-require_once __DIR__ . '/includes/class-mysat-surpl.php';
+foreach ( glob( plugin_dir_path( __FILE__ ) . "includes/*.php" ) as $file ) {
+	require_once $file;
+}
+
 
 // This help to create additional button after Plugin it is activated Installed Plugins list
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'mysat_custom_link_options_plugin', 10, 2);
@@ -113,18 +144,21 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'mysat_custom_lin
 function mysat_custom_link_options_plugin($actions): array
 {
 
-    // Build URL Links
-    $settings = esc_url(add_query_arg('page', 'my-style-anytime', get_admin_url() . 'admin.php'));
-    $getpro = esc_url(add_query_arg('', '', 'https://newfiesoft.com/wp-plugins/my-style-anytime/'));
 
-    // Links name
-    $support_name ='Settings';
-    $getpro_name ='Get Premium';
+ // Build URL Links
+ $support_url = esc_url(add_query_arg('page', 'my-style-anytime', get_admin_url() . 'admin.php'));
+ $getpro_url = esc_url(add_query_arg('', '', 'https://newfiesoft.com/wp-plugins/my-style-anytime/'));
 
 
-    // Create buttons
-    $settings_url = '<a href="' . $settings . '">' . $support_name . '</a>';
-    $site_link_premium = '<a href="' . $getpro . '" class="premium" target="_blank">' . $getpro_name . '</a>';
+ // Links name
+ $support_name =__('Settings', 'my-style-anytime');
+ $getpro_name =__('Get Premium', 'my-style-anytime');
+
+
+ // Create buttons
+ $settings_url = '<a href="' . $support_url . '">' . $support_name . '</a>';
+ $site_link_premium = '<a href="' . $getpro_url . '" class="premium" target="_blank">' . $getpro_name . '</a>';
+
 
     // Organize buttons
     return array_merge(
@@ -145,20 +179,41 @@ function mysat_custom_link_action_plugin($links_array, $mysat_plugin_name)
 
     if ($mysat_plugin_name === $plugindir) {
 
+
  // Build URL Links
  $support_url = 'https://wordpress.org/support/plugin/my-style-anytime/';
- $plugin_faq_url = 'https://wordpress.org/plugins/my-style-anytime/#faq';
+ $faq_url = 'https://wordpress.org/plugins/my-style-anytime/#faq';
+ $rating_url = 'https://wordpress.org/support/plugin/my-style-anytime/reviews/#new-post';
+
 
  // Links name
- $support_name ='Community Support';
- $plugin_faq_name ='FAQ';
+ $support_name =__('Community Support', 'my-style-anytime');
+ $faq_name =__('FAQ', 'my-style-anytime');
+ $rating_name =__('Ratings', 'my-style-anytime');
+
 
  // Create buttons
  $links_array[] = '<a href="' . $support_url . '" class="help-style" target="_blank">' . $support_name . '</a>';
- $links_array[] = '<a href="' . $plugin_faq_url . '" class="help-style" target="_blank">' . $plugin_faq_name . '</a>';
+ $links_array[] = '<a href="' . $faq_url . '" class="help-style" target="_blank">' . $faq_name . '</a>';
+ $links_array[] = '<a href="' . $rating_url . '" class="help-style" target="_blank">' . $rating_name . '</a>';
 
     }
-
     return $links_array;
+
 }
 
+// This register hook who checks DISALLOW_FILE_EDIT and set it to true if need before deactivating the plugin
+register_deactivation_hook( __FILE__, 'mysat_deactivate_plugin' );
+
+function mysat_deactivate_plugin() {
+	$config_file = ABSPATH . 'wp-config.php';
+	$config = file_get_contents( $config_file );
+
+	if (preg_match("/define\('DISALLOW_FILE_EDIT',\s*(false|'false')\);/i", $config)) {
+		$config = preg_replace("/define\('DISALLOW_FILE_EDIT',\s*(false|'false')\);/i", "define('DISALLOW_FILE_EDIT', true);", $config);
+	} elseif (!preg_match("/define\('DISALLOW_FILE_EDIT',/i", $config)) {
+		file_put_contents($config_file, "\n\ndefine('DISALLOW_FILE_EDIT', true);", FILE_APPEND);
+	}
+
+	file_put_contents($config_file, $config);
+}
--- readme.txt
+++ readme.txt
@@ -3,13 +3,14 @@ Contributors: NewfieSoft
 Donate link: https://newfiesoft.com/donate
 Tags: style, css, custom-css, styles, stylesheet, editor, custom, customizer, admin, admin-style, frontend, frontend-style, backend, backend-style, rules, user, user-rules, user-style
 Requires at least: 4.9.0
-Tested up to: 6.1
-Stable tag: 1.0.3
+Tested up to: 6.1.1
+Stable tag: 1.0.4
 Requires PHP:  5.2.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html 
 
-=== Description ===
+
+== Description ==
 
 This plugin helps you to create and customize user types of rules using <strong>CSS style sheets</strong>.
 
@@ -21,6 +22,8 @@ At the same time, you can do responsive design on the same CSS file rule.
 
 This <strong>Lite Version</strong> plugin supports all standard [WordPress user roles](https://wordpress.org/support/article/roles-and-capabilities/).
 
+From version <strong>1.0.4</strong> we add functionality to disable Gutenberg style anywhere & enable the classic editor. This function helps you to back your WordPress in a classic view, in posts, pages, and widgets.
+
 ### Features
 
 * Administrator custom style view
@@ -29,39 +32,17 @@ This <strong>Lite Version</strong> plugin supports all standard [WordPress user 
 * Contributor custom style view
 * Subscriber custom style view
 * Including Visitor/Guest custom style view style.
+* Disable Gutenberg style
 
 
-While this <strong>[Premium Version](https://newfiesoft.com/wp-plugins/my-style-anytime/)</strong>, we work to integrate other additional independent WordPress plugins user rules such as [WooCommerce](https://wordpress.org/plugins/woocommerce/), [Loco Translate](https://wordpress.org/plugins/loco-translate/), [Yoast SEO](https://wordpress.org/plugins/wordpress-seo/), and many others.
+While this <strong>[Premium Version](https://newfiesoft.com/wp-plugins/my-style-anytime/)</strong>, we work to integrate other additional independent WordPress plugins user rules such as [WooCommerce](https://wordpress.org/plugins/woocommerce/), [Loco Translate](https://wordpress.org/plugins/loco-translate/), [Yoast SEO](https://wordpress.org/plugins/wordpress-seo/), [YITH WooCommerce Affiliates](https://wordpress.org/plugins/yith-woocommerce-affiliates/), and many others.
 
 
-=== Usage ===
-
-1. Install
-2. Activate
-2. Go to My Style Anytime
-3. From the left side of the menu you have Manage Style, click on that you go on plugin style files editors.
-5. Enjoy in your job... :)
-
-=== Frequently Asked Questions ===
-
-= Is this plugin free? =
-
-Yes, this plugin version is 100% free.
-
-= Can I show / include it in Frontend public/visitor/guest style? =
-
-Yes, of course, in any rules style you can find the next part of the code commented.
-
-/* To import visitor style in this user @Rules type
-@import "visitor-style.css";
- */
-
-When you enable import visitor-style.css to need to know, you import all that is inside in visitor-style.css
-
-=== Screenshots ===
+== Screenshots ==
 
 1. Welcome screen
-2. Manage Style
+2. Plugins Settings where you can enable additional useful functions
+3. Code Editor where you can manage and customize your style
 3. Administrator custom style view
 4. Editor custom style view
 5. Author custom style view
@@ -69,25 +50,72 @@ When you enable import visitor-style.css to need to know, you import all that is
 7. Subscriber custom style view
 8. Visitor custom style view
 
-=== Changelog ===
 
-= 1.0.3 =
-# Release Date: 03.11.2022
-[*] Testing code with WordPress version 6.1
+== Installation ==
 
-= 1.0.2.9 =
-# Release Date: 01.10.2022
-[*] Fix bug after update in version 1.0.2
+= Simple Modern Way =
+1. Go to the WordPress Dashboard, from the <strong>Plugins</strong> menu you can see Add New click on that.
+2. On the right side, you can see the Search field. In that field enter <strong>my style anytime</strong>.
+3. Click on <strong>Install Now</strong>, then <strong>Activate</strong>.
 
-= 1.0.2 =
-# Release Date: 01.10.2022
-[*] We optimized the code and added additional buttons on the Installed Plugins list page itself.
+= Manual Old Way =
 
-= 1.0.1 =
-# Release Date: 01.09.2022
-[*] We add a function where visitors have the owner's custom style (visitor-style.css). With this now you do not need to use theme style.css primary.
+1. Unzip the downloaded zip file
+2. Upload the plugin folder into the <strong>wp-content/plugins/</strong> directory of your WordPress site.
+3. Go to the <strong>WordPress Dashboard</strong>, and click on <strong>Plugins</strong> on the list of plugins you will see <strong>My Style Anytime</strong> from the Plugins page.
+4. Click on <strong>Activate</strong>.
+
+= After Install =
+
+Now on the left menu, you have new options with the name <strong>My Style Anytime</strong>, click on the name how would see all options.
+
+On the plugin menu you have <strong>Manage Style</strong>, click on that you go on plugin style files editors.
+
+Enjoy your work... 🥳
 
 
-= 1.0.0 =
-# Release Date: 01.08.2022
-[*] First release
+== Frequently Asked Questions ==
+
+= Is this plugin free? =
+
+Yes, this plugin version is 100% free.
+
+= Can I show or include it in Frontend public/visitor/guest style? =
+
+Yes, of course, in any rules style, you can find the next part of the code commented.
+
+/* To import visitor style in this user @Rules type
+@import "visitor-style.css";
+ */
+
+When you enable import visitor-style.css to need to know, you import all that is inside in visitor-style.css
+
+= Backup important files when you upgrade to the latest version =
+
+If you manually upload the plugin on the last version using someone files  FTP application, only just skip and not remove folder  <strong>styles</strong>, all others you can remove and all from the last version that you download.
+
+But if you're using an upgrade inside the WordPress site plugin area, before doing that just backup folder <strong>styles</strong>, and after doing that you can upgrade to the official last version.
+
+
+== Changelog ==
+
+= 1.0.4 - 24.02.2023 =
+* We reorganized the directory structure and code.
+* Add : Settings pages where you can enable or disable on-click file editing.
+* Add : Functions on Settings page where you disable Gutenberg style anywhere & Enable classic editor.
+* Add : From this version, the .pot file is a part of the plugin and is 100% optimize with the plugin. That means now we can make localization and work on multi Languages support.
+
+= 1.0.3 - 01.10.2022 =
+* Add : Ratings on the Installed Plugins list page itself.
+
+= 1.0.2.9 - 01.10.2022 =
+* Fix : the bug after the update in version 1.0.2
+
+= 1.0.2 - 01.10.2022 =
+* We optimized the code and added additional buttons on the Installed Plugins list page itself.
+
+= 1.0.1 - 01.09.2022 =
+* We add a function where visitors have the owner's custom style (visitor-style.css). With this now you do not need to use theme style.css primarily.
+
+= 1.0.0 - 01.08.2022 =
+* First release
